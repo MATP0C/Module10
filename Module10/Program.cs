89af840eba4ecbc1b676f39f0c43472627fc26be
bogdan <bogdan.kuznetsov2021@yandex.ru>
2023-05-21 10:06:51 +0300

    Добавьте файлы проекта.

---
+++ Module10/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Module10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public interface IManager
+    {
+        void Create()
+        {
+
+        }
+        void Read()
+        {
+
+        }
+        void Update()
+        {
+
+        }
+        void Delete()
+        {
+
+        }
+    }
+    public class Manager : IManager
+    {
+        public void Create()
+        {
+
+        }
+
+        public void Read()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Delete()
+        {
+
+        }
+    }
+}
