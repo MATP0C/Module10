3ad07a0f3d41bb739111808f2da0d9ad44fef139
bogdan <bogdan.kuznetsov2021@yandex.ru>
2023-05-21 11:09:17 +0300

    10.3.1

--- Module10/Program.cs
+++ Module10/Program.cs
@@ -4,20 +4,36 @@ namespace Module10
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            var worker = new Worker();
-            ((IWorker)worker).Build();
             Console.ReadKey();
         }
     }
-    public interface IWorker
+    public interface IWriter
     {
-        public void Build();
+        void Write();
     }
-    public class Worker : IWorker
+
+    public interface IReader
     {
-        void IWorker.Build()
+        void Read();
+    }
+
+    public interface IMailer
+    {
+        void SendEmail();
+    }
+    public class FileManager : IWriter, IReader, IMailer
+    {
+        void IWriter.Write()
+        {
+
+        }
+        void IReader.Read()
+        {
+
+        }
+        void IMailer.SendEmail()
         {
 
         }
