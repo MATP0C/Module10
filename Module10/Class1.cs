31751c9044a6c2584d204128d72dfcfd5312fa28
bogdan <bogdan.kuznetsov2021@yandex.ru>
2023-05-24 11:29:51 +0300

    10.5.1

---
+++ Module10/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module10
+{
+    public class Calculator1 : ICalculator
+    {
+        ILogger Logger { get;}
+        public Calculator1(ILogger logger)
+        {
+            Logger = logger;
+        }
+        public void Sum()
+        {
+            Logger.Event("Калькулятор начал свою работу...");
+            Thread.Sleep(3000);
+            Logger.Event("Введите первое число");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Logger.Event("Введите второе число");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Logger.Event("Результат вычислений:");
+            double total = a + b;
+            Console.WriteLine("{0} + {1} = {2}", a, b, total);
+            return;
+        }
+    }
+}
--- Module10/Program.cs
+++ Module10/Program.cs
@@ -1,41 +1,72 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Schema;
 
 namespace Module10
 {
     class Program
     {
-        static void Main()
+        static ILogger Logger { get; set; }
+        static void Main(string[] args)
         {
-            var user = new User();
-            var account = new Account();
-
-            IUpdater<Account> updater = new UserService();
-
-            var userService = new UserService();
-            userService.Update(user);
-            Console.ReadKey();
+            Logger = new Logger();
+            var calculator1 = new Calculator1(Logger);
+            while(true)
+            {
+                try
+                {
+                    calculator1.Sum();
+                }
+                catch (ArgumentNullException)
+                {
+                    Logger.Error("Аргумент передоваемый в метод не имеет значения");
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    Logger.Error("Аргумент находится за пределами диапозона допустимых значений");
+                }
+                catch (OverflowException)
+                {
+                    Logger.Error("Арифметическая операция приводит к переполнению");
+                }
+                catch (FormatException)
+                {
+                    Logger.Error("Значение не находится в соответсвующем формате");
+                }
+                finally
+                {
+                    Console.WriteLine("Повторите попытку снова");
+                }
+            }
         }
     }
-    public class User
-    {
 
+    public interface ICalculator
+    {
+        void Sum();
     }
 
-    public class Account : User
+    public interface ILogger
     {
-
+        void Event(string message);
+        void Error(string message);
     }
 
-    public interface IUpdater<in User>
+    public class Logger : ILogger
     {
-        void Update(User entity);
-    }
-
-    public class UserService : IUpdater<User>
-    {
-        public void Update(User entity)
+        public void Error(string message)
         {
-
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+        public void Event(string message)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine(message);
+            Console.ResetColor();
         }
     }
 }
